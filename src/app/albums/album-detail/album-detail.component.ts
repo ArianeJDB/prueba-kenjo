a423b08fafe15498a0154dfa8854dec58fd3f3e2
arianejdb <ariane.jurado@gmail.com>
2020-10-10 18:58:51 +0200

    detail filter and connection with edit page

--- src/app/albums/album-detail/album-detail.component.ts
+++ src/app/albums/album-detail/album-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Albums } from 'src/app/models/albums.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-album-detail',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./album-detail.component.scss']
 })
 export class AlbumDetailComponent implements OnInit {
-
-  constructor() { }
+  albums: Albums[];
+  albumSelected: Albums[];
+  albumData: {};
+  id: string;
+  constructor( private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.albums = JSON.parse(localStorage.getItem('albums'));
+    this.id = this.activatedRoute.snapshot.params.id;
+    this.filterAlbum()
   }
 
+  filterAlbum() {
+    this.albumSelected = this.albums.filter(album => album._id === this.id);
+
+  }
 }
--- src/app/albums/albums-list/albums-list.component.ts
+++ src/app/albums/albums-list/albums-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { AlbumbsService } from 'src/app/albumbs.service';
@@ -9,9 +9,11 @@ import { Albums } from 'src/app/models/albums.model';
   templateUrl: './albums-list.component.html',
   styleUrls: ['./albums-list.component.scss']
 })
-export class AlbumsListComponent implements OnInit {
+export class AlbumsListComponent implements OnInit, OnDestroy {
 
   albums$: Observable<Albums[]>;
+  albums: Albums[];
+  sub: Subscription;
 
   constructor(
     private service: AlbumbsService,
@@ -19,8 +21,29 @@ export class AlbumsListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.albums$ = this.service.getAlbums()
+    this.albums$ = this.service.getAlbums();
+    this.sub = this.albums$.subscribe(
+      (response) => {
+        this.albums = response;
+        localStorage.setItem('albums', JSON.stringify(response));
+      },
+      (error) => console.log(error)
+    );
   }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    }
+
+
+
+
+
+
+
+
+
+
 
   goToDetail(id): any {
     this.router.navigate(['albums/detail/' + id]);
